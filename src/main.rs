105f8c26d36edccd44e87c60a212915905e44a44
nacl42 <code@sreblov.de>
2020-12-27 00:13:17 +0100

    added stub for clap command-line support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, crate_version, Arg};
+use clap_generate::{generate, generators::Bash};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("merula")
+        .version(crate_version!())
+        .author("nacl42 <code@sreblov.de>")
+        .about("simple cli frontend to access merula files (.mr)")
+        .arg("-v --verbose... 'Sets the verbosity level'")
+        .subcommand(
+            App::new("list")
+                .about("list memos")
+                .arg("<input> 'sets an input file'")
+        );
+
+    let matches = app.get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("list") {
+        // read memos from .mr file into database
+        // TODO
+        if let Some(input) = matches.value_of("input") {
+            //init.logger(matches.occurences_of("verbose") as u8);
+            println!("pretending to load input file '{}'", input);
+        }        
+    }
 }
